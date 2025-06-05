b55aef626df0fa2ccea9ccff5e4e182c733b898f
Fabio Ferreira da Silva <fabio_ferreiradasilva@yahoo.com.br>
2025-06-05 15:19:40 -0300

    Iniciando o projeto

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+client = TestClient(app)
+
+
+def test_root_deve_retornar_ok_e_ola_mundo():
+    response = client.get("/")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"message": "Olá Mundo!"}
